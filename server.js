d972ba82263523a5f7213e9357a37202eb391a0d
Payam Dowlatyari <pdowlatyari@gmail.com>
2020-04-25 17:54:12 -0700

    push to heroku

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('./models/db');
-
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyparser = require('body-parser');
-
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/your-app-name');
 const employeeController = require('./controllers/employeeController');
 
 var app = express();
@@ -16,8 +17,11 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(3000, () => {
-    console.log('Express server started at port : 3000');
-});
+// app.listen(3000, () => {
+//     console.log('Express server started at port : 3000');
+// });
+
+const port = process.env.PORT || 3000;
+app.listen(port);
 
 app.use('/employee', employeeController);
